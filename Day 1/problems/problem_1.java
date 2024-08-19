8dceffdaf78e1ce6e620acf41f8f3616ac4589d1
Utkarsh <kaduutkarsh52@gmail.com>
2024-08-20 00:56:34 +0530

    first commit

---
+++ Day 1/problems/problem_1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class problem_1 {
+    // Push at the Bottom of the Stack
+
+    public class Stack {
+        public static void pushAtBottom(Stack<Integer> s, int data) {
+         if()
+        }
+    }
+}
